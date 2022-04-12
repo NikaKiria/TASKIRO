84b611ae7aaef452fb247657ed1febc51bb29cd7
NikaKiria <knika508@gmail.com>
2022-04-12 19:50:26 +0400

    add unique flag to email

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -3,7 +3,7 @@ import * as mongoose from 'mongoose';
 export const userSchema = new mongoose.Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   groups: Array,
   profession: { type: String, required: true },
