f456b04b13f283743a707f64315375c33b97450c
NikaKiria <knika508@gmail.com>
2022-04-12 19:47:54 +0400

    fix mongodb atlas connection problem caused by path of env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserController } from './user/user.controller';
+import { userModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '../src/env/.env',
+      envFilePath: './src/env/.env',
       isGlobal: true,
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
+      inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URI'),
       }),
-      inject: [ConfigService],
     }),
+    userModule,
   ],
-  controllers: [UserController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
