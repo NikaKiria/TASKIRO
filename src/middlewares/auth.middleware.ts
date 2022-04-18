81fb030b324c0088cc1f2adb3580730d169e8ee8
NikaKiria <knika508@gmail.com>
2022-04-18 21:40:36 +0400

    fix request object problem

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -6,18 +6,20 @@ import * as jwt from 'jsonwebtoken';
 export class authMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     try {
-      const token = req.header('Authorization');
+      const token = req.header('Authorization').split(' ')[1];
       if (!token) {
         throw new HttpException('Authorization failed!', 500);
       }
+      // Verify token
       const verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
       if (!verifiedToken) {
         throw new HttpException('Authorization denied!', 400);
       }
-      req['userEmail'] = verifiedToken;
+      // Assign user email to request
+      req.userEmail = verifiedToken;
       next();
     } catch (err) {
-      throw new HttpException('Something went wrong!', 500);
+      throw new HttpException('Something went wrong in authorization!', 500);
     }
   }
 }
