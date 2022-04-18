2999c3d4a7d6d8dd623a8375c2d637b255f0567a
NikaKiria <knika508@gmail.com>
2022-04-18 21:35:19 +0400

    import auth  middleware to check if user is authenticated or not

--- src/groups/group.module.ts
+++ src/groups/group.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { authMiddleware } from 'src/middlewares/auth.middleware';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
 import { groupSchema } from './models/group.model';
@@ -11,4 +12,8 @@ import { groupSchema } from './models/group.model';
   controllers: [GroupController],
   providers: [GroupService],
 })
-export class groupModule {}
+export class groupModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(authMiddleware).forRoutes('/groups');
+  }
+}
