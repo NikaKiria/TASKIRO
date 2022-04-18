c6d676091958946063972987a7e0f6d52b086caa
NikaKiria <knika508@gmail.com>
2022-04-18 21:38:47 +0400

    fix validation

--- src/groups/dto/createGroup.dto.ts
+++ src/groups/dto/createGroup.dto.ts
@@ -1,25 +1,20 @@
-import { IsNotEmpty, Matches, Length } from 'class-validator';
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty, Length } from 'class-validator';
 
 export class createGroupDto {
   @IsNotEmpty({ message: 'Group title must not be empty!' })
   @Length(3, 30, { message: 'Group title must be 3-30 characters long!' })
-  @Matches('/[A-Za-zds]/g', undefined, {
-    message: 'Group title must only contain alphanumeric and space characters!',
-  })
   title: string;
 
   @IsNotEmpty({ message: 'Group description must not be empty!' })
-  @Length(3, 30, { message: 'Group description must be 3-30 characters long!' })
-  @Matches('/[A-Za-zds]/g', undefined, {
-    message:
-      'Group description must only contain alphanumeric and space characters!',
+  @Length(3, 300, {
+    message: 'Group description must be 3-300 characters long!',
   })
   description: string;
 
   @IsNotEmpty({ message: 'Group must have members!' })
   members: Array<string>;
 
-  @IsNotEmpty({ message: 'Group must have author!' })
   author: string;
 }
 
