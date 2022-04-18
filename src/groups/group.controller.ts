3f3a45bd18892d55de601ee82832c55c8c0dcfe9
NikaKiria <knika508@gmail.com>
2022-04-18 21:33:21 +0400

    create create-group route and import needed modules

--- src/groups/group.controller.ts
+++ src/groups/group.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  Body,
+} from '@nestjs/common';
+import { createGroupDto } from './dto/createGroup.dto';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { GroupService } from './group.service';
 
 @Controller('groups')
-export class GroupController {}
+export class GroupController {
+  constructor(private GroupService: GroupService) {}
+
+  @Post('create-group')
+  @HttpCode(201)
+  @UsePipes(ValidationPipe)
+  createGroup(@Body() providedGroupObject: createGroupDto) {
+    return this.GroupService.createGroup(providedGroupObject);
+  }
+}
