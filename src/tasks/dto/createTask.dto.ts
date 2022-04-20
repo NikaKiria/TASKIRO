2f8121dfea81eb55ea9812ce1904cd6d53cec264
NikaKiria <knika508@gmail.com>
2022-04-20 19:55:34 +0400

    create createTask dto to validate provided object

---
+++ src/tasks/dto/createTask.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, Length } from 'class-validator';
+
+export class createTaskDto {
+  @IsNotEmpty({ message: 'Task title must not be empty!' })
+  @Length(3, 40, { message: 'Task title must be 3-40 characters long!' })
+  title: string;
+
+  @IsNotEmpty({ message: 'Task description must not be empty!' })
+  @Length(3, 300, {
+    message: 'Task description must be 3-300 characters long!',
+  })
+  description: string;
+}
+
+export interface Task {
+  title: string;
+  description: string;
+}
