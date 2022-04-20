864a3b8a5dc8702b86a952e467451cf1189968b2
NikaKiria <knika508@gmail.com>
2022-04-20 19:56:21 +0400

    create task model using mongoose

---
+++ src/tasks/models/task.model.ts
@@ -0,0 +1,9 @@
+import * as mongoose from 'mongoose';
+
+export const taskSchema = new mongoose.Schema({
+  assignee: { type: String, required: true },
+  userAssignedTo: { type: String, required: true },
+  groupId: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+});
