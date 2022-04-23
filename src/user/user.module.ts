0a5727fbfbc9fdf63ff1cfce0348801dce2b8d5c
NikaKiria <knika508@gmail.com>
2022-04-23 13:08:30 +0400

    import auth middleware in taks module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { userSchema } from './models/user.model';
+import { authMiddleware } from 'src/middlewares/auth.middleware';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: userSchema }])],
   controllers: [UserController],
   providers: [UserService],
 })
-export class userModule {}
+export class userModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(authMiddleware).forRoutes('/user/profile');
+  }
+}
